33349f3732ad77877e5189c16e0405e92127f9cb
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-06-13 11:43:16 +0200

    Finished the book library

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-
-let addNewBookButton = document.querySelector(".addBook");
+let addNewBookButton = document.querySelector(".addBookButton");
 let popUpForm = document.querySelector(".popUp");
 let closeButton = document.querySelector(".close");
 let tableBody = document.querySelector(".tableBody");
@@ -17,6 +16,7 @@ addNewBookButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     popUpForm.style.display = "none";
+    form.reset();
   });
 
 
@@ -31,19 +31,10 @@ function Book(title, author, pages, status) {
 }
 
 
-/*if (localStorage.getItem('books') === null) {
-    myLibrary = [];
-  } else {
-    const booksFromStorage = JSON.parse(localStorage.getItem('books'));
-    myLibrary = booksFromStorage;
-  } */
-
   function updateLocalStorage() {
     localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    //myLibrary = JSON.parse(localStorage.getItem("library"));
   }
 
-  //updateLocalStorage();
 
   const getStorage = () => {
     myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
@@ -51,49 +42,59 @@ function Book(title, author, pages, status) {
 
 
 
-function addBookToLibrary(title, author, pages, status) {
+function addBookToLibrary() {
     const book = new Book(titleInput.value, authorInput.value, pagesInput.value, statusInput.value);
     myLibrary.push(book);
     updateLocalStorage();
     displayBooks();
-    clearForm();
+    form.reset();
 }
-
-//addBookToLibrary();
-
 
 //addBookToLibrary("LotR", "Tolkien", "500", "read");
 //addBookToLibrary("Harry Potter", "Rowling", "400", "unread");
 
 
-console.table(myLibrary);
+//console.table(myLibrary);
 
 
 function displayBooks() {
  //return myLibrary.map(item => item.title);
-  tableBody.innerHTML = "";
-  /*const bookList = document.querySelector(".tableBody");
-  bookList.textContent = ''; */
+  const bookList = document.querySelector(".tableBody");
+  bookList.textContent = ''; 
   for (let i = 0; i < myLibrary.length; i++) {
     const bookRow = document.createElement('tr');
     bookList.appendChild(bookRow);
-    // BOOK TITLE
+    // Book title
     const bookTitle = document.createElement('td');
     bookTitle.textContent = myLibrary[i].title;
     bookRow.appendChild(bookTitle);
-    // BOOK AUTHOR
+    // Book author
     const bookAuthor = document.createElement('td');
     bookAuthor.textContent = myLibrary[i].author;
     bookRow.appendChild(bookAuthor);
-    // BOOK PAGES
+    // Book pages
     const bookPages = document.createElement('td');
     bookPages.textContent = myLibrary[i].pages;
     bookRow.appendChild(bookPages);
-    // BOOK STATUS BUTTON
+    // Book status button
     const bookStatus = document.createElement('td');
-    bookStatus.textContent = myLibrary[i].status;
+    const statusButton = document.createElement('button');
+    statusButton.className = "statusButton";
+    statusButton.textContent = myLibrary[i].status;
+    bookStatus.appendChild(statusButton);
     bookRow.appendChild(bookStatus);
-    // BOOK REMOVAL BUTTON
+
+    statusButton.addEventListener("click", () => {
+        if (myLibrary[i].status === "read") {
+            myLibrary[i].status = "unread";
+          } else {
+            myLibrary[i].status = "read";
+            }
+        updateLocalStorage();
+        displayBooks();
+      });
+
+    // Book removal button
     const bookDelete = document.createElement('td');
     const deleteButton = document.createElement('button');
     deleteButton.className = "deleteButton";
@@ -103,18 +104,12 @@ function displayBooks() {
 
     deleteButton.addEventListener("click", () => {
         myLibrary.splice(i, 1);
-        //bookList.removeChild(bookRow);
         updateLocalStorage();
         displayBooks();
       });
   }
  }
 
-
-
-  function clearForm() {
-   form.reset();
-  }
 
 
   form.addEventListener("submit", (e) => {
